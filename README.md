2ebe6708c5aa81471d26038d94955fe763105174
gitviini <gvinicius105@gmail.com>
2024-01-29 18:56:23 -0300

    up

---
+++ README.md
@@ -0,0 +1,11 @@
+## CatCat
+
+by: [@v.ii.n.i](https://instagram.com/v.ii.n.i)
+
+#7DaysOfCode
+
+    Website made for training of JS
+
+<img src='static/catcat.png' width='100%'>
+
+version: 1.1.0
--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('desafio1.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
