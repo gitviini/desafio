a1e2488058aa2f7dfe076205e424166e6aa30866
gitviini <gvinicius105@gmail.com>
2024-02-01 11:59:31 -0300

    up

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template, make_response
+from os import listdir
+
+listdir()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('desafio1.html')
+    return render_template('operações.html')
 
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
 
+@app.route('/<quest_name>')
+def quest(quest_name=''):
+    try:
+        return render_template(f'{quest_name}.html')
+    except:
+        return 'page not found'
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
