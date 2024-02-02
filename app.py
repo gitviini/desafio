f82045ad9071f488847c0565f9538283052a4eba
gitviini <gvinicius105@gmail.com>
2024-02-02 16:50:12 -0300

    up quests

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, make_response
 from os import listdir
 
+fouder = 'quests/'
+
 listdir()
 
 app = Flask(__name__)
@@ -12,9 +14,9 @@ def index():
 @app.route('/<quest_name>')
 def quest(quest_name=''):
     try:
-        return render_template(f'{quest_name}.html')
+        return render_template(fouder+f'{quest_name}.html')
     except:
-        return 'page not found'
+        return render_template('error.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000, host='0.0.0.0')
